1df360b4053ee6f9b8fe1965be1a5e3459d014e7
halildemiroz <hlldmrz007@gmail.com>
2025-10-01 14:44:01 +0300

    .

--- assets/tilemap/tileset.tsx
+++ assets/tilemap/tileset.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="tileset" tilewidth="32" tileheight="32" tilecount="96" columns="12">
- <image source="../../assets/craftpix-net-846754-free-green-zone-tileset-pixel-art/Tileset.png" width="384" height="256"/>
+ <image source="Tileset.png" width="384" height="256"/>
  <tile id="42">
   <objectgroup draworder="index" id="3">
    <object id="27" x="-0.363636" y="31.9977">
